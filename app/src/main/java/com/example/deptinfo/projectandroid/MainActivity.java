78146d873c6233881b0802e985d64bafe54b5318
deptinfo <diallohawaou310@gmail.com>
2019-01-02 16:07:22 +0100

    ajout du menu

--- app/src/main/java/com/example/deptinfo/projectandroid/MainActivity.java
+++ app/src/main/java/com/example/deptinfo/projectandroid/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.deptinfo.projectandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import  android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
 }
